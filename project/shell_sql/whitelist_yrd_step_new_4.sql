474bd2603dc797209d760430ac71cf840458bbbb
liruiqi123 <1595854626@qq.com>
2018-06-20 15:40:55 +0800

    新添加规则 在所有业务线条出现过最高逾期天数（单次最高非累加）≥30天的逾期

---
+++ project/shell_sql/whitelist_yrd_step_new_4.sql
@@ -0,0 +1,10 @@
+CREATE TABLE  whitelist_yrd_step_new_1 AS
+SELECT k.*,if(k.tmp_1 is null,1,0) as new_1_flag
+FROM
+(SELECT
+a.*,b.id_number as tmp_1
+FROM
+(select k.* from  whitelist_yrd_source_all k where  max_overdue_days >= 30)  a
+LEFT JOIN
+whitelist_yrd_other_product_source b
+ON a.id_number = b.id_number)k
