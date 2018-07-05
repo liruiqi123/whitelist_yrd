aee18320a461aa4aea83d1cc9fe619239f64864a
liruiqi123 <1595854626@qq.com>
2018-07-05 14:14:46 +0800

    获取进件用户当前月和首期还款月的期数

--- project/shell_sql/whitelist_yrd_step_old_2.sql
+++ project/shell_sql/whitelist_yrd_step_old_2.sql
@@ -1,13 +1,15 @@
 #对应规则：间隔6个月以内（含），拒绝  （7做6不做）
 #应该设置表名的参数
 
-
-
-select months_between('2017-05-01','2017-03-01') ;
-
-
-select *,months_between('2018-07-06',substr(cashier_time,0,10)) AS tmp_old_2_1,
-from whitelist_yrd_step_old_1 ;
-
-
+select
+k.*,
+months_between('2018-07-06',substr(k.cashier_time,0,10)) AS tmp_old_2_1,
+months_between('2018-07-06',substr(k.due_date,0,10)) AS tmp_old_2_2
+from
+(SELECT  a.*,b.due_date  from
+whitelist_yrd_step_old_1 a
+LEFT  JOIN
+whitelist_yrd_source_firstReturn b
+on
+a.transport_id = b.transport_id)k
 
