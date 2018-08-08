cc8bb4fddd1403394066c53d9f781617b0b022ce
liruiqi123 <1595854626@qq.com>
2018-08-08 16:14:12 +0800

    在八方征信数据中提取进件人的信用卡开发信息

---
+++ project/shell_sql/whitelist_yrd_source_openDate.sql
@@ -0,0 +1,24 @@
+#创建数据表，沉淀近两期发生过逾期的客户
+#在八方征信数据中提取进件人的信用卡开发信息
+
+
+db=whitelist_yrd
+#db=$2
+#sre_db=$3
+
+table1=whitelist_yrd_openDate
+
+
+
+hive -e " use $db;
+
+drop table $table1;
+
+
+create table $table1 as
+select a.pboc_rep_id,a.open_date,b.id_number
+from
+ys_dw.dw_crp_loan_detail a,
+ys_dw.dw_crp_info b
+where a.pboc_rep_id = b.pboc_rep_id
+"
--- project/shell_sql/whitelist_yrd_step_old_5.sql
+++ project/shell_sql/whitelist_yrd_step_old_5.sql
@@ -13,6 +13,8 @@ max_re_s
 table1=whitelist_yrd_step_old_5_1_rec_ov_days
 table2=whitelist_yrd_step_old_5_1_rec_ov_days
 table3=whitelist_yrd_step_old_5_3_max_re_st
+table4=whitelist_yrd_step_old_5_3_max_re_st_grade
+
 
 source_table=whitelist_yrd_source_12_term
 
@@ -23,10 +25,11 @@ hive -e " use $db;
 drop table $table1;
 drop table $table2;
 drop table $table3;
+drop table $table4
 
 
 CREATE  TABLE   $table1 as
-select if (if(max(overdue_days) = 'null',0, max(overdue_days))>10,10,if(max(overdue_days) = 'null',0, max(overdue_days))),apply_id
+select if (if(max(overdue_days) = 'null',0, max(overdue_days))>10,10,if(max(overdue_days) = 'null',0, max(overdue_days))) as rec_ov_days ,apply_id
 from $source_table
 group by
 apply_id;
@@ -37,6 +40,18 @@ CREATE  TABLE   $table3 as
 select  * ,months_between('2018-07-06',substr(apply_time,0,10)) as max_re_st
 from whitelist_yrd_source_all;
 
+
+
+
+CREATE TABLE $table4 AS SELECT *,
+    CASE
+        WHEN floor(max_re_st) <= 12 THEN 0.33611
+        WHEN floor(max_re_st) = 13 THEN 0.16843
+        WHEN floor(max_re_st) = 14 THEN -0.15021
+		WHEN (floor(max_re_st) >= 15 and floor(max_re_st)<=18)  THEN -0.18566
+        WHEN floor(max_re_st) >= 19 THEN -0.5808
+    END AS max_re_st_grade FROM
+    $table3;
 
 
 
